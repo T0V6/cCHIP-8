38528b643c1ec86cbab8d3b831974a61b1662d95
TV08 <89317713+TV08@users.noreply.github.com>
2024-02-29 02:59:39 -0300

    feat: added second type of opcodes

--- include/chip.h
+++ include/chip.h
@@ -67,6 +67,55 @@ void op_7xnn(Chip* obj);
 
 
 
-/* */
+/* LD (load register) 
+    * Sets Vx register with the contents of Vy register
+*/
+void op_8xy0(Chip* obj);
+
+/* OR (bitwise | operation)
+    * Sets Vx register to Vx OR Vy
+*/
+void op_8xy1(Chip* obj);
+
+/* AND (bitwise & operation) 
+    * Sets Vx register to Vx AND Vy
+*/
+void op_8xy2(Chip* obj);
+
+/* XOR (bitwise ^ operation) 
+    * Sets Vx register to Vx XOR Vy
+*/
+void op_8xy3(Chip* obj);
+
+/* ADD (add registers with carry)
+    * Adds Vx and Vy and stores into Vx
+    * If result is greater than 255 (1B), VF is set to 1 and only the lowest 8 bits are kept
+*/
+void op_8xy4(Chip* obj);
+
+/* SUB (subtract registers)
+    * Subtracts Vx and Vy and stores into Vx
+    * If Vx is greater than Vy, VF is set to 1
+*/
+void op_8xy5(Chip* obj);
+
+/* SHR (shift right)
+    * Shifts 1 bit right Vx register
+    * Least significant bit is stored in VF register
+    * If LSB is 1, VF is set to 1
+*/
+void op_8xy6(Chip* obj);
+
+/* SUBN (subtract registers negative)
+    * Subtracts Vy and Vx and stores into Vx
+    * If Vy is greater than Vx, VF is set to 1
+*/
+void op_8xy7(Chip* obj);
+
+/* SHL (shift left)
+    * Shifts 1 bit left Vx register
+    * MSB is stored in VF register
+*/
+void op_8xye(Chip* obj);
 
 #endif // CHIP_H
--- src/chip.c
+++ src/chip.c
@@ -88,7 +88,7 @@ void op_3xnn(Chip* obj)
     uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
 
     // Doesn't need shifting since it's the whole second byte
-    uint8_t nn = (obj->opcode & 0x00FFU);
+    uint8_t nn = obj->opcode & 0x00FFU;
 
     if (obj->regs[vx] == nn)    { obj->reg_pc += 2; }
 
@@ -136,3 +136,108 @@ void op_7xnn(Chip* obj)
 }
 
 
+
+
+void op_8xy0(Chip* obj)
+{
+    uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy = (obj->opcode & 0x00F0U) >> 4U;
+
+    obj->regs[vx] = obj->regs[vy]; // *((*obj).regs + vy); lol
+
+    return;
+}
+
+void op_8xy1(Chip* obj)
+{
+    uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy = (obj->opcode & 0x00F0U) >> 4U;
+
+    obj->regs[vx] |= obj->regs[vy];
+
+    return;
+}
+
+void op_8xy2(Chip* obj)
+{
+    uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy = (obj->opcode & 0x00F0U) >> 4U;
+
+    obj->regs[vx] &= obj->regs[vy];
+
+    return;
+}
+
+void op_8xy3(Chip* obj)
+{
+    uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy = (obj->opcode & 0x00F0U) >> 4U;
+
+    obj->regs[vx] ^= obj->regs[vy];
+
+    return;
+}
+
+void op_8xy4(Chip* obj)
+{
+    uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy = (obj->opcode & 0x00F0U) >> 4U;
+    
+    uint16_t result = obj->regs[vx] + obj->regs[vy];
+
+    obj->regs[CC8_SIZE_REGISTERS - 1] = (result > 255U) ? 1 : 0;
+
+    obj->regs[vx] = result & 0x00FFU;
+
+    return;
+}
+
+void op_8xy5(Chip* obj)
+{
+    uint8_t vx = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy = (obj->opcode & 0x00F0U) >> 4U;
+
+    obj->regs[CC8_SIZE_REGISTERS - 1] = (obj->regs[vx] > obj->regs[vy]) ? 1 : 0;
+
+    obj->regs[vx] -= obj->regs[vy];
+
+    return;
+}
+
+void op_8xy6(Chip* obj)
+{
+    uint8_t vx  = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t lsb = obj->regs[vx] & 0x1U; // 0x1 == 0001
+
+    obj->regs[CC8_SIZE_REGISTERS - 1] = lsb;
+
+    obj->regs[vx] >>= 1;
+
+    return;
+}
+
+void op_8xy7(Chip* obj)
+{
+    uint8_t vx  = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t vy  = (obj->opcode & 0x00F0U) >> 4U;
+
+    uint16_t sub = obj->regs[vy] - obj->regs[vx];
+
+    obj->regs[CC8_SIZE_REGISTERS - 1] = (sub > 0) ? 1 : 0;
+
+    obj->regs[vx] = sub;
+
+    return;
+}
+
+void op_8xye(Chip* obj)
+{
+    uint8_t vx  = (obj->opcode & 0x0F00U) >> 8U;
+    uint8_t msb = (obj->regs[vx] & 0x80U) >> 7U; // 1000 0000 --> 0000 0001
+
+    obj->regs[CC8_SIZE_REGISTERS - 1] = msb;
+
+    obj->regs[vx] <<= 1;
+
+    return;
+}
